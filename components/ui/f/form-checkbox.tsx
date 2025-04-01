ec63245f74d7c9ca7fdbf3c1fda2aac734b23dc4
Chris Gambrell <christopher@gambrell.info>
2025-04-01 11:29:02 -0400

    Update default checked

--- components/ui/f/form-checkbox.tsx
+++ components/ui/f/form-checkbox.tsx
@@ -11,21 +11,15 @@ export function FormCheckbox({
 	name,
 	state,
 	...props
-}: React.ComponentProps<typeof CheckboxPrimitive.Root> & {
-	clearOnError?: boolean
-	id: string
-	label: React.ReactNode | string
-	name: string
-	state?: ActionState
-}) {
+}: React.ComponentProps<typeof CheckboxPrimitive.Root> & { clearOnError?: boolean; label: React.ReactNode | string; state?: ActionState }) {
 	const id = String(name ?? _id)
-	const value = clearOnError ? '' : state?.values?.[id] ?? ''
+	const checked = clearOnError ? false : state?.values?.[id] === 'true' ? true : false
 	const error = state?.fieldErrors?.[id]?.[0]
 
 	return (
 		<div className='grid gap-2'>
 			<div className='flex items-center gap-2'>
-				<Checkbox id={id} name={id} defaultValue={value} {...props} />
+				<Checkbox id={id} name={id} defaultChecked={checked} {...props} />
 				<Label htmlFor={id}>{label}</Label>
 			</div>
 			<FormError value={error ? [error] : []} />
